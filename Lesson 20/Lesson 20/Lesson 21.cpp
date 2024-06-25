4f4272789458c65f17b48ab5f1bf2d6cbe69fd8c
Subarashill <taob8@outlook.com>
2024-06-26 01:29:12 +0300

    Lesson 20

---
+++ Lesson 20/Lesson 20/Lesson 21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+	short A, B;
+
+	cout << "Enter the first number: " << endl;
+	cin >> A;
+
+	cout << "Enter the second number: " << endl;
+	cin >> B;
+
+	A++;// this is Increment and it's add 1 to the value and you can write it as ++A too
+	--B;// this is Decrement and it's take 1 from the value and you can write it as B-- too
+
+	cout << "the first number after increment: " << A << "the second number after decrement :" << B << endl;
+
+
+
+
+
+
+
+	return 0;
+}
