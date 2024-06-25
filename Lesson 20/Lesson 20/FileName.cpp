00f43d846b5c7ae84680edccfefc849c096b8585
Subarashill <taob8@outlook.com>
2024-06-25 18:14:57 +0300

    Lesson 20

---
+++ Lesson 20/Lesson 20/FileName.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+// examples on the the Arthmetic Operators
+
+	short A = 10, B = 20;
+
+	cout << "A + B =" << A + B << endl;
+	cout << "A - B =" << A - B << endl;
+	cout << "A * B =" << A * B << endl;
+	cout << "A / B =" << A / B << endl;
+	cout << "A % B =" << A % B << endl;
+
+	return 0;
+}
